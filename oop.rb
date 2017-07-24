a0e79a76ad36860102631278cd55156a330e3870
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-24 09:30:26 -0400

    oop tests passing

--- oop.rb
+++ oop.rb
@@ -4,15 +4,24 @@
 # - A `greet` instance method
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
+class Animal
 
+  attr_accessor :name
 
+  def initialize name
+    @name = name
+  end
+
+  def greet
+  end
+
+end
 
 
 # Question 7
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
-
-
+pumba = Animal.new("Pumba")
 
 
 # Question 8
@@ -22,11 +31,19 @@
 # - A `king` (Boolean) attribute
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
+class Lion < Animal
 
-
-
-
+  def initialize name
+    super(name)
+    if name == "Simba"
+      @king = true
+    else
+      @king = false
+    end
+  end
+end
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+simba = Lion.new("Simba")
