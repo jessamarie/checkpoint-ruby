8c18fffdc80dc982c84dde3a405cbaa075caa8b9
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-24 09:19:21 -0400

    fundamentals tests passing

--- fundamentals.rb
+++ fundamentals.rb
@@ -3,11 +3,9 @@
 # When called the method should print to the terminal:
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
-
-
-
-
-
+def offer_rose person
+  puts "Would you take this rose #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 # Question 2
 # Assume the following hash...
@@ -25,10 +23,8 @@ town = {
 # - Remove "Belle" from `residents`
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
-
-
-
-
+town[:residents].delete_at(1)
+town[:castle][:guests].push("Belle")
 
 
 
@@ -43,3 +39,6 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+for i in 0..friends.length
+  puts "Belle is friends with #{friends[i]}"
+end
